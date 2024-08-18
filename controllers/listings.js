f8c1ad2d7e90813d9c53fc865d12e2f5ebeae97b
Vishal Thakur <vishalthakur0378958@gmail.com>
2024-08-18 12:45:03 +0530

    root page added

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,7 +3,7 @@ const Listing = require("../models/listing");
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
-  res.render("listings/", { allListings });
+  res.render("listings/index", { allListings });
 };
 
 module.exports.renderNewForm = (req, res) => {
